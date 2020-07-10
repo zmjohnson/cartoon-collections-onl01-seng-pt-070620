4d3f60934f0000cad20df5812c68a87312714524
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-10 22:49:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,9 @@ def summon_captain_planet(array)
   end 
   nu_array
 end
-
-summon_captain_planet(planeteer_calls)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+summon_captain_planet(planeteer_calls)
+
 
 def long_planeteer_calls(array) 
   i = 0 
