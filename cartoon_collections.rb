ae1385a348ee5db8f4f66d09cd61dfb4f00482b0
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-10 22:44:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,13 +9,17 @@ end
 
 roll_call_dwarves(dwarves)
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+summon_captain_planet(planeteer_calls)
 
 def find_the_cheese# code an argument here
   # the array below is here to help
